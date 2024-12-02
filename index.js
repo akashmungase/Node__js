a008e27c29d22c043c78a7279928418f34e78fb5
Akash <akashmungase99@gmail.com>
2024-12-02 10:02:31 +0530

    public folder

--- index.js
+++ index.js
@@ -72,5 +72,37 @@ const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname, 'public');
 
-app.use(express.static(publicPath));
-app.listen(3000);
+// app.use(express.static(publicPath));
+
+// app.get('', (req, res) => {
+//     res.sendFile(`${publicPath}/index.html`)
+// })
+
+// app.get('/about', (req, res) => {
+//     res.sendFile(`${publicPath}/about.html`)
+// })
+
+// app.get('/contact', (req, res) => {
+//     res.sendFile(`${publicPath}/contact.html`)
+// })
+
+// app.get('*', (req, res) => {
+//     res.sendFile(`${publicPath}/404.html`)
+// })
+
+app.set('view engine', 'ejs')
+
+app.get('/profile', (req, res) => {
+    const user = {
+        name: 'Akash',
+        email: 'akashmungase@test.com',
+        skills: ['HTML', 'CSS', 'Bootstrap', 'JavaScript']
+    }
+    res.render('profile', {user})
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.listen(8000);
