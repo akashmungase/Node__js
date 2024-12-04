d2335fa26edeb531a651d62465df02a3dd995564
Akash <akashmungase99@gmail.com>
2024-12-05 00:20:36 +0530

    get post api called

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dbConnect = require('./mongodb');
 const app = express();
 
-// app.use(express.json);
+app.use(express.json());
 
 app.get('/', async (req, res) => {
     let data = await dbConnect();
@@ -12,7 +12,31 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
     let data = await dbConnect();
-    let result = await data.insertMany(req.body);
+    let result = await data.insertOne(req.body);
+    res.send(result);
+})
+
+app.put('/', async (req, res) => {
+    let data = await dbConnect();
+    let result = await data.updateOne(
+        {name: req.body.name}, {$set: req.body}
+    );
+    res.send(result);
+})
+
+app.put('/:name', async (req, res) => {
+    let data = await dbConnect();
+    let result = await data.updateOne(
+        {name: req.params.name}, {$set: req.body}
+    );
+    res.send(result);
+})
+
+const { ObjectId } = require('mongodb');
+
+app.delete('/:id', async (req, res) => {
+    let data = await dbConnect();
+    let result = await data.deleteOne({ _id: new ObjectId(req.params.id) });
     res.send(result);
 })
 
